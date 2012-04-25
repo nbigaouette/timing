d63bcfa6f76529995dc8374cddd1c5485906efbd
Nicolas Bigaouette <nbigaouette@gmail.com>
2012-04-24 20:35:18 -0400

    Give 4 more characters for the counter print.

--- src/Timing.cpp
+++ src/Timing.cpp
@@ -100,12 +100,12 @@ namespace timing
         timer_name_w_spaces = timer_name;
         timer_name_w_spaces.resize(longest_length, ' ');
 
-        log("%s| %s | %10.5g | %13.6g | %8llu | %6.2f |\n", s.c_str(),
-                                                            timer_name_w_spaces.c_str(),
-                                                            timer.Get_Duration(),
-                                                            timer.Get_Duration() / double(nt),
-                                                            timer.Get_Counter(),
-                                                            (timer.Get_Duration() / TimerTotal.Get_Duration())*100.0);
+        log("%s| %s | %10.5g | %13.6g | %12llu | %6.2f |\n", s.c_str(),
+                                                             timer_name_w_spaces.c_str(),
+                                                             timer.Get_Duration(),
+                                                             timer.Get_Duration() / double(nt),
+                                                             timer.Get_Counter(),
+                                                             (timer.Get_Duration() / TimerTotal.Get_Duration())*100.0);
     }
 
     // **********************************************************
@@ -132,7 +132,7 @@ namespace timing
         std::string total_human_readable("Total (human readable)");
         const std::string timings("Timing of different code aspects");
         longest_length = std::max(longest_length, total_human_readable.length());
-        const size_t total_length_minus_longest = 51;
+        const size_t total_length_minus_longest = 55;
         const size_t total_length = total_length_minus_longest + longest_length; // Does not include the first and last "|"
 
         // Center the table inside the terminal width
@@ -177,15 +177,15 @@ namespace timing
             Print_N_Times(" ", length_right, false);
             log("|");
         }
-        log("          Duration          | Nb times | Total  |\n");
+        log("          Duration          | Number times | Total  |\n");
 
         log("%s|", s.c_str());
         Print_N_Times(" ", longest_length+2, false);
-        log("|  seconds   | per time step | called   |   %c    |\n", '%');
+        log("|  seconds   | per time step |    called    |   %c    |\n", '%');
 
         log("%s|", s.c_str());
         Print_N_Times("-", longest_length+2, false);
-        log("|------------|---------------|----------|--------|\n");
+        log("|------------|---------------|--------------|--------|\n");
 
         for (std::map<std::string, Timer>::iterator it = TimersMap.begin() ; it != TimersMap.end(); ++it )
         {
@@ -194,17 +194,17 @@ namespace timing
 
         log("%s|", s.c_str());
         Print_N_Times("-", longest_length+2, false);
-        log("|------------|---------------|----------|--------|\n");
+        log("|------------|---------------|--------------|--------|\n");
 
         // Print total last
         Print_Code_Aspect(s, TimerTotal, "Total", longest_length, nt);
 
         log("%s|", s.c_str());
         Print_N_Times("-", longest_length+2, false);
-        log("|------------------------------------------------|\n");
+        log("|----------------------------------------------------|\n");
 
         total_human_readable.resize(longest_length, ' ');
-        log("%s| %s | %46s |\n", s.c_str(), total_human_readable.c_str(), TimerTotal.Duration_Human_Readable().c_str());
+        log("%s| %s | %50s |\n", s.c_str(), total_human_readable.c_str(), TimerTotal.Duration_Human_Readable().c_str());
 
         log("%s|", s.c_str());
         Print_N_Times("-", total_length, false);
