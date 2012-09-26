599cc487c517c74c3c879feefa9143414a95a796
Nicolas Bigaouette <nbigaouette@gmail.com>
2012-09-26 19:57:19 -0400

    Add time steps per second example.

--- example/Main.cpp
+++ example/Main.cpp
@@ -27,6 +27,8 @@ int main(int argc, char *argv[])
     // Starting time is 0.0, end time is 3*max_t (two rounds of cosines and one of sines)
     static timing::Eta ETA = timing::Eta(0.0, double(3*max_t));
 
+    static timing::TimestepTiming timestep_timing;
+
     // This will create a new timing object called "cosine".
     TIMER_START("cosine", Timing_Cosine);
     for (int t = 0 ; t < max_t ; t++)
@@ -37,7 +39,7 @@ int main(int argc, char *argv[])
 
         // Print only 10 ETA, else we are flooded...
         if (t % (max_t/10) == 0)
-            std::cout << "ETA: " << ETA.Get_ETA(double(t)) << "\n";
+            std::cout << "ETA: " << ETA.Get_ETA(double(t)) << "  Timesteps/second: " << timestep_timing.Timesteps_per_Second(t) << "\n";
     }
     TIMER_STOP("cosine", Timing_Cosine);
 
@@ -51,7 +53,7 @@ int main(int argc, char *argv[])
 
         // Print only 10 ETA, else we are flooded...
         if (t % (max_t/10) == 0)
-            std::cout << "ETA: " << ETA.Get_ETA(double(max_t + t)) << "\n";
+            std::cout << "ETA: " << ETA.Get_ETA(double(max_t + t)) << "  Timesteps/second: " << timestep_timing.Timesteps_per_Second(t) << "\n";
     }
     TIMER_STOP("sine", Timing_Sine);
 
@@ -66,13 +68,23 @@ int main(int argc, char *argv[])
 
         // Print only 10 ETA, else we are flooded...
         if (t % (max_t/10) == 0)
-            std::cout << "ETA: " << ETA.Get_ETA(double(max_t + max_t + t)) << "\n";
+            std::cout << "ETA: " << ETA.Get_ETA(double(max_t + max_t + t)) << "  Timesteps/second: " << timestep_timing.Timesteps_per_Second(t) << "\n";
     }
     TIMER_STOP("cosine", Timing_Cosine2);
 
     TIMER_START("Wait", Timing_Wait);
     timing::Wait(2.123);
     TIMER_STOP("Wait", Timing_Wait);
+
+    // Test the time steps per second
+    for (int t = 0 ; t < 10 ; t++)
+    {
+        // Each iteration should state 1.5 seconds
+        timing::Wait(1.5);
+        std::cout << "ETA: " << ETA.Get_ETA(double(t)) << "   ";
+        std::cout << "timestep/second = " << timestep_timing.Timesteps_per_Second(t);
+        std::cout << "\n";
+    }
 
     timing::Print(max_t);
 
