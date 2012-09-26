d5549b16f21b44c72d2d3da21438ffd55538a4d7
Nicolas Bigaouette <nbigaouette@gmail.com>
2012-09-26 19:27:29 -0400

    For backward compatibility, define Seconds_per_Timestep() and Timesteps_per_Second() with time as argument, but don't use that argument.

--- src/Timing.hpp
+++ src/Timing.hpp
@@ -212,11 +212,14 @@ namespace timing
             TimestepTiming();
 #ifndef DISABLE_TIMING
             inline double Seconds_per_Timestep(const uint64_t t, const double time) { return _Seconds_per_Timestep(t); }
+            inline double Seconds_per_Timestep(const uint64_t t)                    { return _Seconds_per_Timestep(t); }
             inline double Timesteps_per_Second(const uint64_t t, const double time) { return _Timesteps_per_Second(t); }
             inline double Timesteps_per_Second(const uint64_t t)                    { return _Timesteps_per_Second(t); }
 #else // #ifndef DISABLE_TIMING
             inline double Seconds_per_Timestep(const uint64_t t, const double time) { /* Don't do anything */                }
+            inline double Seconds_per_Timestep(const uint64_t t)                    { /* Don't do anything */                }
             inline double Timesteps_per_Second(const uint64_t t, const double time) { /* Don't do anything */                }
+            inline double Timesteps_per_Second(const uint64_t t)                    { /* Don't do anything */                }
 #endif // #ifndef DISABLE_TIMING
     };
 
